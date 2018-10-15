3b5b0aea6612eda4c6dd8e628fb513bcb4e531f3
C Reid <charlesreid1@Gmail.com>
2018-10-15 11:48:54 -0700

    update simple api

---
+++ api/Readme.md
@@ -0,0 +1,12 @@
+# Fly API
+
+This directory contains a tiny API called the Fly API. (It's tiny like a fly.)
+
+This API defines a few kinds of endpoints:
+
+* An endpoint that receives a JSON payload
+* An endpoint that sends a JSON payload
+* A page that uses an API endpoint internally
+
+
+
--- api/flyapi.py
+++ api/flyapi.py
@@ -2,12 +2,12 @@ from flask_api import FlaskAPI
 
 app = FlaskAPI(__name__)
 
-@app.route('/example/')
-def example():
+@app.route('/example/api/endpoint')
+def api_out():
     return {'hello': 'world'}
 
-@app.route('/example/')
-def example():
+@app.route('/example/api/receiver')
+def api_in():
     return {'request data': request.data}
 
 
